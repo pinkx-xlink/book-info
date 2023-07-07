fa5f708870e1e0cd9e336eaf1d6e5fb80d8f47b5
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-06 20:53:56 -0400

    And const book1

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ function Book(title, author, pages, read) {
     this.author = author 
     this.pages = pages
     this.read = read
+    this.sayBook = function() {
+        console.log(Book)
+    }
 }  
+
+const book1 = new Book('Deep Trouble', 'R.L. Stine', 117, read)
+
+
+book1.sayBook()
