4e92ba2afe4cb53d855ebf58c5c210d5b2e8036c
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-09 22:11:20 -0400

    Add more js framework for Odin Project assignment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author 
@@ -8,7 +10,17 @@ function Book(title, author, pages, read) {
     }
 }  
 
+function addBookToLibrary(){
+    // the constructor...
+}
+
 const deepTrouble = new Book('Deep Trouble', 'R.L. Stine', 117, read)
 
 
 console.log(deepTrouble.info());
+
+
+
+
+
+
