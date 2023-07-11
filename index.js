7a6d57933ab6b7a003c4b102fd681b4691445bad
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-11 19:49:19 -0400

    Add remove Button

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function render() {
             <div class="card-body">
                 <p>${book.pages} pages</p>
                 <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+                <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
             </div>
         `;
         libraryElement.appendChild(bookElement);
