be9095de8c3dae5af1b456d704c83990bc696a27
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-10-31 22:20:32 -0400

    Troubleshoot, fix indent problems

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    //this.info = function() {
-    //    console.log(Book)
-    //}
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+//   this.info = function() {
+//       console.log(Book)
+//   }
 }  
 
 Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+  this.read = !this.read;
 }
 
 function toggleRead(index) {
-    myLibrary[index].toggleRead();
-    render()
+  myLibrary[index].toggleRead();
+  render()
 }
 
 function render() {
-    let libraryElement = document.querySelector("#library");
-    libraryElement.innerHTML = "";
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i];
-        let bookElement = document.createElement("div");
-        bookElement.setAttribute("class", "book-card");
-        bookElement.innerHTML = `
+  let libraryElement = document.querySelector("#library");
+  libraryElement.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookElement = document.createElement("div");
+    bookElement.setAttribute("class", "book-card");
+    bookElement.innerHTML = `
             <div class="card">
                 <h3 class="title">${book.title}</h3>
                 <h5 class="author">by: ${book.author}</h5>
@@ -37,44 +37,38 @@ function render() {
                 <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
             </div>
         `;
-        libraryElement.appendChild(bookElement);
-    }
+    libraryElement.appendChild(bookElement);
+  }
 }
 
 function removeBook(index) {
-    myLibrary.splice(index, 1);
-    render();
+  myLibrary.splice(index, 1);
+  render();
 }
 
 
 function addBookToLibrary(){
-    let title = document.querySelector("#title").value; //maybe remove #?
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").checked;
-    let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
-    myLibrary.push(newBook);
-    render();
-    // do stuff here
+  let title = document.querySelector("#title").value; //maybe remove #?
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
+  console.log(newBook);
+  myLibrary.push(newBook);
+  render();
+  // do stuff here
 }
 
 let newBookBtn = document.querySelector("#new-book-btn");
 newBookbtn.addEventListener("click", function() {
-    let newBookForm = document.querySelector("#new-book-form");
-    newBookForm.style.display = "block";
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
 })
 
 document.getElementById("#new-book-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    addBookToLibrary();
+  event.preventDefault();
+  addBookToLibrary();
 })
 
 // const book1 = new Book('Deep Trouble', 'R.L. Stine', 117, 'read')
 //console.log(deepTrouble.info());
-
-
-
-
-
-
