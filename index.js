a9be6cc8e5ad75eaf59d96dcae8f1fb9827e6ed9
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-11-09 10:10:38 -0500

    Add filler to saveBookBtn

--- index.js
+++ index.js
@@ -82,6 +82,10 @@ document.getElementById("#new-book-form").addEventListener("submit", function(ev
 })
 
 let saveBookBtn = document.querySelector("#save-book-btn");
+saveBookBtn.addEventListener("click", function() {
+  let = document.querySelector("# ");
+  .style.display = "block";
+})
 
 // const book1 = new Book('Deep Trouble', 'R.L. Stine', 117, 'read')
 //console.log(deepTrouble.info());
